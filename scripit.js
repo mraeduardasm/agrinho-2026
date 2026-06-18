2e7ef06857a2ed6ff2a4b5390a17c70082fbd45b
mraeduardasm <me267520@gmail.com>
2026-06-18 09:23:22 -0300

    Add function to display sustainability message

---
+++ scripit.js
@@ -0,0 +1,4 @@
+function mostrarMensagem() {
+   document.getElementById("mensagem").innerText =
+       "Pequenas atitudes, como economizar água, reciclar e usar energias limpas, ajudam a construir um futuro sustentável!";
+}
